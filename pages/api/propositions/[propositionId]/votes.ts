36668485e3b52839c880f676109ab2294f48c09b
Christophe GUILLAUME <christophe.g@homeexchange.com>
2023-11-13 11:27:18 +0100

    project finish

--- pages/api/propositions/[propositionId]/votes.ts
+++ pages/api/propositions/[propositionId]/votes.ts
@@ -1,18 +1,14 @@
-import { Board, Proposition } from '@prisma/client';
+import { Vote } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 import { prisma } from '~/src/db/prisma';
 
 type Data = {
-  proposition: Proposition;
+  vote: Vote;
 };
 
-const BodyScheme = z.object({
-  title: z.string().min(1).max(255),
-});
-
 const QueryScheme = z.object({
-  boardId: z.string().transform((id) => Number(id)),
+  propositionId: z.string().transform((id) => Number(id)),
 });
 
 export default async function handler(
@@ -25,15 +21,13 @@ export default async function handler(
   }
 
   const query = QueryScheme.parse(req.query);
-  const body = BodyScheme.parse(JSON.parse(req.body));
 
-  const proposition = await prisma.proposition.create({
+  const vote = await prisma.vote.create({
     data: {
-      title: body.title,
-      boardId: query.boardId,
       ip: String(Math.random()),
+      propositionId: query.propositionId,
     },
   });
 
-  res.status(201).json({ proposition });
+  res.status(201).json({ vote });
 }
